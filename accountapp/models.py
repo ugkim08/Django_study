e85f12bb2753c926cf056e2ebe19436cad83900e
ugkim08 <wigk0118@gmail.com>
2021-07-27 22:41:41 +0900

    Django class 14

--- accountapp/models.py
+++ accountapp/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+
+class HelloWorld(models.Model):
+    text = models.CharField(max_length=255, null=False) # null = false는 text변수가 없어도 되는지 설정하는 파라미터 필수값으로 설정하려면 false로. 선택값으로 설정하려면 ture로
