8c1b410883fb484c2baacd3cb62c1d6520b5db1d
ugkim08 <wigk0118@gmail.com>
2021-07-28 22:21:18 +0900

    Django class 16

--- accountapp/views.py
+++ accountapp/views.py
@@ -7,4 +7,9 @@ from django.shortcuts import render
 
 
 def hello_world(request):
-    return render(request, 'accountapp/hello_world.html')
+
+    if request.method == "POST":
+        # context={} >> data 꾸러미의 일종으로 POST로 request를 받으면 POST METHOD!!!라는 이름의 text데이터를 render해주라는 의미..
+        return render(request, 'accountapp/hello_world.html', context={'text': 'POST METHOD!!!'})
+    else:
+        return render(request, 'accountapp/hello_world.html', context={'text': 'GET METHOD!!!'})
