3eb50b00b3f3571360ae90fc80120b31377e773d
ugkim08 <wigk0118@gmail.com>
2021-08-01 14:18:59 +0900

    Django class 18 explane code

--- accountapp/views.py
+++ accountapp/views.py
@@ -27,7 +27,9 @@ def hello_world(request):
         # 16강 // context={} >> data 꾸러미의 일종으로 POST로 request를 받으면 POST METHOD!!!라는 이름의 text데이터를 render해주라는 의미
         # 17강 // hello_world_input에 입력된 text를 POST로 받을 경우 new_hello_world 변수로 보내 저장한다.
         # 18강 // 웹페이지를 새로고침 했을 때 마지막 입력값이 계속 나오는걸 막기 위해 HttpResponseaRedirect를 사용해서 해당 이슈를 막아야 함.
-        # 18강 // reverse 함수를 활용해서 accountapp:hello_world의 주소를 자동으로 만들어 줘야 함.
+        # 18강 // reverse 함수를 활용해서 accountapp:hello_world의 주소를 자동으로 입력해 줘야 함.
+            # 18강 // reverse 함수는 장고 urls.py 파일 urlpatterns에서 정의된 이름을 입력하면 자동으로 해당 url을 연결시켜 줌.
+                    # 13번 라인부터 33번 라인 코드를 읽어보면  hello_world_input에 입력된 값을 POST로 accountapp models.py의 HelloWorld를 통해 DB에 추가한 뒤 HttpResponseRedirect 함수와 reverse함수를 이용해 accountapp urls.py에서 정의된 hello_world의 이름을 가진 url로 보낸다.
         return HttpResponseRedirect(reverse('accountapp:hello_world'))
     else:
         hello_world_list = HelloWorld.objects.all()
