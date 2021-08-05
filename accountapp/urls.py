cd3e298f0283694b4a924aad8ad4f7e4cfb9c8a0
ugkim08 <wigk0118@gmail.com>
2021-08-05 23:39:31 +0900

    Django class 22

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
 from accountapp.views import hello_world, AccountCreateView
@@ -9,6 +10,10 @@ app_name = "accountapp"
 urlpatterns = [
     # 21강 // 11번 라인은 함수형 veiw에서 쓰이는 타입
     path('hello_world/', hello_world, name='hello_world'),
+
+    path('login/', LoginView.as_view(template_name='accountapp/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+
     # 21강 // 13번 라인은 Class형 view에서 쓰이는 타입. 여기서 AccountCreateView는 accountapp view파일에 있는 AccountCreateView 함수임.
     path('create/', AccountCreateView.as_view(), name='create'),
 ]
